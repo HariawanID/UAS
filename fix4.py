4862ca804eaaedb11a7f614f6bbc8f0523727a4d
HariawanID <hariawan.ind@gmail.com>
2022-11-27 06:29:25 +0700

    Update fix4.py

--- fix4.py
+++ fix4.py
@@ -6,7 +6,7 @@ import cv2
 def pencilSketch(input_image):
     grey = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY) # Meng convert gambar menjadi abu abu
     grey = cv2.medianBlur(grey, 5) #membuat gambar blurry
-    edges = cv2.adaptiveThreshold(grey, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7,9) #membuat sketch
+    edges = cv2.adaptiveThreshold(grey, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7,7) #membuat sketch
     color = cv2.bilateralFilter(input_image, d=9, sigmaColor=200,sigmaSpace=200)
     # cartoon = cv2.bitwise_and(color, color, mask=edges)
 
